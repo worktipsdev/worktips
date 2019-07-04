21359a215b8618b5725f05d01b1fe46e005bb0e9
worktipsdev <48468699+worktipsdev@users.noreply.github.com>
2019-07-04 20:26:44 +0530

    Update service_node_rules.cpp

--- src/cryptonote_core/service_node_rules.cpp
+++ src/cryptonote_core/service_node_rules.cpp
@@ -19,7 +19,7 @@ uint64_t get_staking_requirement(cryptonote::network_type m_nettype, uint64_t he
 
   uint64_t height_adjusted = height - hardfork_height;
   uint64_t base = 0, variable = 0;
-  if (height > 100 && hf_version >= cryptonote::network_version_11_infinite_staking )
+  if (height > 100)
 	  {
     base     = 200000 * COIN;
     variable = (250007.0 * COIN) / worktips::exp2(height_adjusted/6480000.0);
